8778b588f5a25a11097ce407319d353c164f4fb4
Daksh Sethi <dakshsethi1604@gmail.com.com>
2024-01-09 03:09:07 +0530

    sets up server and login/signup connection

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Login from "./Components/SignIn/Login.jsx"
+import SignUp from "./Components/SignUp/SignUp.jsx"
+
+function App(){
+
+
+    return <div>
+        <SignUp />
+    </div>
+}
